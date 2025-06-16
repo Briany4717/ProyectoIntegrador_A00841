2d239921f4ff828700b280b86a4f34638ef3ac89
Brianyan4717 <132787816+Brianyan4717@users.noreply.github.com>
2025-06-15 22:22:47 -0600

    Release 5

--- Episodio.cpp
+++ Episodio.cpp
@@ -15,8 +15,7 @@
  * @param _genero The genre of the episode.
  */
 #include "Episodio.h"
-Episodio::Episodio(const int _id, string _nombre,const int _duration, string _genero):
-    Video(_id,std::move(_nombre),_duration,std::move(_genero)){};
+Episodio::Episodio(const int _id, string _nombre,const int _duration, string _genero): Video(_id,std::move(_nombre),_duration,std::move(_genero)){};
 
 /**
  * @brief Method to present the episode's details including title, duration, and rating.
@@ -27,6 +26,5 @@ Episodio::Episodio(const int _id, string _nombre,const int _duration, string _ge
  */
 void Episodio::Presentar() {
         cout << id << ".- Titulo: " << nombre
-                 << " Duración: " << FormatDuration()
-                 << " Calificación: " << GetCalificacion() << endl;
+                 << " Duración: " << FormatDuration() << " Calificación: " << GetCalificacion() << endl;
     };
